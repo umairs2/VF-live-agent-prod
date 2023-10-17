5b47c923ed42a3cfb1710fcef589604cea12412b
sindrimt <sindrimt@gmail.com>
2023-10-17 08:52:03 +0200

    new test

--- scripts/setup_github_pages.sh
+++ scripts/setup_github_pages.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-git checkout --orphan pages
+git checkout --orphan pages2
 git reset .
 git add -f packages/react-chat/dist
 git add -f packages/widget/dist
 cp scripts/template.html index.html
 git add index.html
 git commit -m "initial" --no-verify
-git push sindrimt pages --force
+git push origin pages2 --force
 git clean -fd .
